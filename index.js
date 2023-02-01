9338a3b5491f092d514ccae65d22782b002b69c7
sanblashdzann <115792662+sanblashdzann@users.noreply.github.com>
2023-02-01 14:30:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Import stylesheets
 import './style.css';
+const http = require('http');
+
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
